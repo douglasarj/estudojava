5df02db341bda0d527c5cc00de0563b301dadba1
Douglas Araujo <156918917+douglasarj@users.noreply.github.com>
2024-11-27 11:19:56 -0300

    Add files via upload

---
+++ MeuAplicativo/src/meuaplicativo/MeuAplicativo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package meuaplicativo;
+
+/**
+ *
+ * @author Douglas53983036
+ */
+public class MeuAplicativo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Aula01 aula01 = new Aula01();
+      aula01.setVisible(true);
+    }
+    
+}
